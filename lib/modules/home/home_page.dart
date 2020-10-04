ac8f1316c085ed244fcfb6755eed6cf690b961aa
abdu ahmed <a01226967080@gmail.com>
2020-10-04 19:51:52 +0200

    home and categories mod

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,129 +1,91 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foood_app/helpers/app_theme.dart';
-import 'package:foood_app/helpers/constant.dart';
-import 'package:foood_app/helpers/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+import 'drawer.dart';
 
-class _HomePageState extends State<HomePage> {
-  int selectedIndex = 0;
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        drawer: DrawerPage(),
+        appBar: AppBar(
+          backgroundColor: AppTheme.primaryColor,
+          centerTitle: true,
+          title: Text(
+            'FOOD',
+            style: GoogleFonts.cairo(letterSpacing: 10, fontSize: 25),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            labelPadding:
+                EdgeInsets.only(left: 18, right: 18, top: 0, bottom: 20),
+            unselectedLabelColor: Colors.red,
+            labelColor: Colors.black,
+            indicatorColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 3,
+            tabs: [
+              Text(
+                'الاطباق الرئيسية',
+                style: GoogleFonts.cairo(
+                  color: AppTheme.fontColor,
+                ),
+              ),
+              Text(
+                'الشوربات والسلطات',
+                style: GoogleFonts.cairo(color: AppTheme.fontColor),
+              ),
+              Text(
+                'الحلويات',
+                style: GoogleFonts.cairo(color: AppTheme.fontColor),
+              ),
+              Text(
+                'المشروبات',
+                style: GoogleFonts.cairo(color: AppTheme.fontColor),
+              ),
+              Text(
+                'المشروبات الساخنة',
+                style: GoogleFonts.cairo(color: AppTheme.fontColor),
+              ),
+              Text(
+                'المشويات',
+                style: GoogleFonts.cairo(color: AppTheme.fontColor),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            DrawerHeader(),
-            ListTile(
-              title: Text(
-                'الرئسية',
-                style: GoogleFonts.cairo(color: AppTheme.fontColor),
-              ),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.homePage);
-              },
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
             ),
-            ListTile(
-              title: Text(
-                'الاقسام',
-                style: GoogleFonts.cairo(color: AppTheme.fontColor),
-              ),
-              onTap: () {
-                Navigator.pushNamed(context, Routes.categoriesPage);
-              },
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
             ),
-            ListTile(
-              title: Text(
-                'البحث',
-                style: GoogleFonts.cairo(color: AppTheme.fontColor),
-              ),
-              onTap: () {},
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
             ),
-            ListTile(
-              title: Text(
-                'المكونات',
-                style: GoogleFonts.cairo(color: AppTheme.fontColor),
-              ),
-              onTap: () {},
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
             ),
-            ListTile(
-              title: Text(
-                'تسجيل الخروج',
-                style: GoogleFonts.cairo(color: AppTheme.fontColor),
-              ),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.loginPage);
-              },
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
+            ),
+            Text(
+              'الرئسية',
+              style: GoogleFonts.cairo(color: AppTheme.fontColor),
             ),
           ],
         ),
-      ),
-      appBar: AppBar(
-        backgroundColor: AppTheme.primaryColor,
-        title: Text(
-          'القائمة ',
-          style: GoogleFonts.cairo(
-              fontSize: 20,
-              letterSpacing: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: Constants.categories.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {
-                      setState(() {
-                        selectedIndex = index;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          Constants.categories[index],
-                          style: GoogleFonts.cairo(
-                              color: selectedIndex == index
-                                  ? AppTheme.fontColor
-                                  : Colors.grey,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Container(
-                          height: 3,
-                          width: 30,
-                          color: selectedIndex == index
-                              ? AppTheme.primaryColor
-                              : Colors.grey,
-                        )
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Expanded(
-            flex: 10,
-            child: Text('hello'),
-          )
-        ],
       ),
     );
   }
